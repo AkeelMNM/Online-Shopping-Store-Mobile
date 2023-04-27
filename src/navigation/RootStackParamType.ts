77e77147b7708d9e5c28af5ab8b9534ca0e69166
Mohamed Akeel <akeel1447@gmail.com>
2023-04-27 22:09:29 +0530

    Adding navigation stack type

---
+++ src/navigation/RootStackParamType.ts
@@ -0,0 +1,9 @@
+export type RootStackParamList = {
+	InitialScreen: undefined;
+	BottomNavBar: undefined;
+	Home: undefined;
+	LogoWithLoaderScreen: undefined;
+	Products: undefined;
+	ShoppingCart: undefined;
+	Settings: undefined;
+};
